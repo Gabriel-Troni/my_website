6184cb15864f807f2cbe79411999fbacb32281a8
Gabriel Alamartini Troni <troni.gabriel.10@gmail.com>
2023-12-07 11:39:18 -0300

    add content

--- slider.js
+++ slider.js
@@ -1,10 +1,24 @@
+var key = true
 var count = 1
 var radioBtn = document.querySelectorAll('.radio-btn')
+var slide = document.getElementsByClassName('slide-box')
 
-setInterval(() => {
-    document.getElementById('radio-btn' + count).checked = true
-    count++
-    if (count > 3) {
-        count = 1
-    }
-}, 7000)
+//slider não está funcionando
+
+slide.addEventListener('mouseover', () => {
+    key = false
+})
+
+slide.addEventListener('mouseout', () => {
+    key = true
+})
+
+if(key) {
+    setInterval(() => {
+        document.getElementById('radio-btn' + count).checked = true
+        count++
+        if (count > 3) {
+            count = 1
+        }
+    }, 7000)
+}
