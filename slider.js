6b58d2b8d416ef1b2affce974e6b231b4477d91b
Gabriel Alamartini Troni <troni.gabriel.10@gmail.com>
2023-12-07 22:25:59 -0300

    add studies content

--- slider.js
+++ slider.js
@@ -1,24 +1,37 @@
-var key = true
+var interval = null
 var count = 1
 var radioBtn = document.querySelectorAll('.radio-btn')
-var slide = document.getElementsByClassName('slide-box')
+var btn1 = document.getElementById('radio-btn1')
+var btn2 = document.getElementById('radio-btn2')
+var btn3 = document.getElementById('radio-btn3')
 
-//slider não está funcionando
-
-slide.addEventListener('mouseover', () => {
-    key = false
-})
-
-slide.addEventListener('mouseout', () => {
-    key = true
-})
-
-if(key) {
-    setInterval(() => {
-        document.getElementById('radio-btn' + count).checked = true
-        count++
-        if (count > 3) {
-            count = 1
-        }
-    }, 7000)
+function startInterval() {
+        interval = setInterval(() => {
+            document.getElementById('radio-btn' + count).checked = true
+            count++
+            if (count > 3) {
+                count = 1
+            }
+        }, 7000)
 }
+
+startInterval()
+
+function delayInterval() {
+    clearInterval(interval)
+    setTimeout(() => {
+        startInterval()
+    }, 30000)
+}
+
+btn1.addEventListener('click', () => {
+    delayInterval()
+})
+
+btn2.addEventListener('click', () => {
+    delayInterval
+})
+
+btn3.addEventListener('click', () => {
+    delayInterval()
+})
