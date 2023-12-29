2fef36d6d6fc8e9c65050d9113c452f1d0480af1
Gabriel Troni <troni.gabriel.10@gmail.com>
2023-12-29 14:04:17 -0500

    Identation upgrade

--- translate.js
+++ translate.js
@@ -1,7 +1,7 @@
 function changeLanguage() {
   var language = document.getElementById('languageSelect').value;
   if (language === 'EN') {
-    window.location.href = 'https://gabriel-troni.vercel.app/EN/';
+    window.location.href = 'https://gabriel-troni.vercel.app/en/';
   } else if (language === 'PTBR') {
     window.location.href = 'https://gabriel-troni.vercel.app/';
   }
